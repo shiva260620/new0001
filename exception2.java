5691152c5941ab782869d62b2682fe194ba0994a
shivam9906 <shivkumarele99@gmail.com>
2023-12-21 12:17:49 +0530

    this is new file

---
+++ exception2.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class test extends RuntimeException
+{
+    test(String msg)
+    {
+        super(msg);
+    }
+}
+public class exception2 {
+    public static void main(String[] args) {
+        public int age;
+        try {
+             Scanner c=new Scanner(System.in);
+             System.out.println("Enter age=");
+            age =c.nextInt();
+            if (age<18)
+                {
+                    throw new test("You are not eligible"+age);
+                }
+            }
+            catch (Exception e) 
+                {
+            System.out.println("You are not eligible");
+                }
+       
+        }
+}
